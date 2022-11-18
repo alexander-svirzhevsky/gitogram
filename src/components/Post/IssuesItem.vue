4ad5b5071ca58ba36c7d670e9385769982a2470f
Aliaksandr Svirzheyski <alexander.svirzhevsky@gmail.com>
2022-11-18 12:45:00 +0300

    added issuesItem component

---
+++ src/components/Post/IssuesItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="issue">
+    <span class="issue__name">{{ name }}</span>
+    <span calss="issue__desc">{{ desc }}</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "IssuesItem",
+  props: {
+    name: {
+      type: String,
+      requered: true
+    },
+    desc: {
+      type: String,
+      requered: true
+    }
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+.issue {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  line-height: 22px;
+  margin-bottom: 3px;
+}
+
+.issue__name {
+  font-weight: 700;
+}
+</style>
--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -4,19 +4,30 @@
     <div class="content">
       <slot name="content"></slot>
     </div>
-    <Toggler></Toggler>
+    <Toggler @onTogglClick="onTogglClick"></Toggler>
+    <ul class="issues" v-if="shown">
+      <li v-for="_ in 3">
+        <IssuesItem name="joshua_l" desc="Enable performance measuring in production, at the user's request">
+        </IssuesItem>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import Profile from "./Profile.vue"
 import Toggler from "./Toggler.vue"
+import IssuesItem from "./IssuesItem.vue"
 
 export default {
   name: "Post",
+  data() {
+    return { shown: false }
+  },
   components: {
     Profile,
-    Toggler
+    Toggler,
+    IssuesItem
   },
   props: {
     profileImg: {
@@ -28,6 +39,11 @@ export default {
       required: true
     }
   },
+  methods: {
+    onTogglClick(value) {
+      this.shown = value
+    }
+  }
 }
 
 </script>
@@ -54,5 +70,10 @@ export default {
   box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.07);
   border-radius: 10px;
   margin-top: 16px;
+  margin-bottom: 18px;
+}
+
+.issues {
+  margin-top: 10px;
 }
 </style>
